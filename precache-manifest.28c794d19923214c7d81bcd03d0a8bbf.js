83fe981f704231f2c3551e484fdcb403af3a0d66
Siddhartha <kwatra22siddhartha@gmail.com>
2020-04-03 15:04:57 +0530

    Updates

---
+++ precache-manifest.28c794d19923214c7d81bcd03d0a8bbf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22352409a0084e6e1194c285058473ab",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "7dd93e976ce791771043",
+    "url": "/Keeper-app/static/js/2.b2465152.chunk.js"
+  },
+  {
+    "revision": "a8e918e76140e83f2e97",
+    "url": "/Keeper-app/static/js/main.605308e0.chunk.js"
+  },
+  {
+    "revision": "2c2b5a6df097cd3a71be",
+    "url": "/Keeper-app/static/js/runtime-main.58bd50c8.js"
+  }
+]);
